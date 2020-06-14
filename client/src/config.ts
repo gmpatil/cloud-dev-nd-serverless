6e30af8d25e28f4e96bcb107792ce2932d5eed8f
gmpatil <>
2020-06-14 16:47:19 -0700

    feat: Added access roles to generateAttachementUpload service to put objects in bucket and update dynamoDB.

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,8 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'vw9tbls8yj'
 //export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
-export const apiEndpoint = `http://localhost:3000/test`
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+//export const apiEndpoint = `http://localhost:3000/test`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
