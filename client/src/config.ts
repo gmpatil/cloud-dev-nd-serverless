fd11b242846e48d07ab07823dfd046f04d0dedd9
gmpatil <>
2020-06-14 21:35:11 -0700

    Make client work for port 3000 instead 5000.

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+// Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = 'vw9tbls8yj'
 //export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
@@ -8,5 +8,6 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-529-qpik.auth0.com',            // Auth0 domain
   clientId: 'S4IXPjfO56QjSXpEOkUVg9ICqFIvsUY0',          // Auth0 client id
-  callbackUrl: 'http://localhost:5000/callback'
+  callbackUrl: 'http://localhost:3000/callback'
+  //callbackUrl: 'http://localhost:5000/callback'  
 }
